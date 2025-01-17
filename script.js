ebca7475bfe6c69d19ee3c8783eba9fa14d44f7a
unknown <glotij17@gmail.com>
2025-01-17 11:49:14 +0300

    ge3

--- script.js
+++ script.js
@@ -1,3 +1,64 @@
+var chooseMenu = "Game Engine 3";
+var language = "en"
+
+var menues = {
+    "menu": {
+        "Game Engine 3": {
+
+        },
+        "Download": {
+
+        },
+        "Control": {
+            
+        },
+        "Nodes": {
+            "Events": {
+
+            },
+            "Loops": {
+
+            },
+            "Text": {
+
+            },
+            "Numbers": {
+
+            },
+            "Logic": {
+
+            },
+            "Objects": {
+
+            },
+            "Another": {
+
+            },
+            "Set": {
+
+            }
+        },
+        "Guides": {
+            "Start": {
+
+            },
+            "First program": {
+
+            },
+            "Variables": {
+
+            },
+            "Project - Dino": {
+
+            }
+        }
+    },
+
+    "menues": {
+        
+    }
+}
+
 function cache(func) {
     const cache = new Map();
 
@@ -10,50 +71,10 @@ function cache(func) {
 
         cache.set(key, func.apply(this, args));
 
-        console.log("Cache miss for key:", key, "Storing result:", cache.get(key));
+        // console.log("Cache miss for key:", key, "Storing result:", cache.get(key));
 
         return cache.get(key);
     };
-}
-
-async function translate(word) {
-    if (word.length === 0) {
-        return "";
-    }
-
-    let point = false;
-
-    if (word[0] === '-') {
-        word = word.slice(1);
-        point = true;
-    }
-
-    let spaces = 0;
-
-    while (word[0] === ' ') {
-        word = word.slice(1);
-        spaces += 1;
-    }
-
-    if (language == "en"){
-        return (point ? '•' : '') + ' '.repeat(spaces) + word;
-    }
-
-    bundle = await cacheGetBundle(language);
-
-    // console.log(word, ' '.repeat(spaces) + word in bundle, word in bundle);
-
-    var answer = "";
-
-    if (' '.repeat(spaces) + word in bundle) {
-        return bundle[' '.repeat(spaces) + word];
-    } else if (word in bundle) {
-        answer = bundle[word];
-    } else {
-        answer = word;
-    }
-
-    return (point ? '•' : '') + ' '.repeat(spaces) + answer;
 }
 
 async function loadJSON(filePath) {
@@ -73,167 +94,98 @@ async function loadJSON(filePath) {
     }
 }
 
-async function getHelpMenu() {
-    help = await cacheLoadJSON("https://raw.githubusercontent.com/artyom7774/Game-Engine-3/main/scr/site/help.json");
+async function holder(text){
+    version = (await cacheLoadJSON("https://raw.githubusercontent.com/artyom7774/Game-Engine-3/main/scr/files/version.json"))["version"];
 
-    version = await cacheLoadJSON("https://raw.githubusercontent.com/artyom7774/Game-Engine-3/main/scr/files/version.json");
-
-    link = `https://github.com/artyom7774/Game-Engine-3/releases/download/GE${version["version"]}/Game.Engine.3.sfx.exe`;
-
-    help["Main"]["pages"]["2"] = {
-        "title": "Downloads",
-        "text": [
-            "Github - <a href='https://github.com/artyom7774/Game-Engine-3'>https://github.com/artyom7774/Game-Engine-3</a>",
-            "Github releases - <a href='https://github.com/artyom7774/Game-Engine-3/releases'>https://github.com/artyom7774/Game-Engine-3/releases</a>",
-            "",
-            `Download - <a href='${link}'>${link}</a>`
-        ]
-    }
-
-    return help;
-}
-
-async function getBundle(language) {
-    bundle = await cacheLoadJSON(`https://raw.githubusercontent.com/artyom7774/Game-Engine-3/main/scr/files/bundles/json/${language}.json`);
-
-    return bundle;
-}
-
-async function loadHelpMenu(menu, submenu) {
-    help = await getHelpMenu()
-
-    // console.log(help)
-
-    now = help[menu];
-
-    for (const key in now["pages"]) {
-        if (now["pages"][key]["title"] === submenu) {
-            now = now["pages"][key];
-            break;
-        }
-    }
-
-    variables = [];
-
-    text = await translate(now["title"])
-
-    variables.push(`<p class="big-help-text">\t${text}</p>`);
-
-    for (const key in now["text"]) {
-        text = await translate(now["text"][key])
-
-        variables.push(`<p class="help-text">${text}</p>`);
-    }
-
-    text = "";
-
-    for (const index in variables) {
-        text += variables[index];
-    }
-
-    text = `<div>` + text + "</div>";
+    text = await text.replace(/%version%/g, version);
 
     return text;
-};
+}
+
+async function menuButtonFunction(menu){
+    chooseMenu = menu;
+
+    initialization();
+}
+
+async function mainInformationView(){
+    const mainClass = document.querySelector(".content__main");
+
+    var text = "";
+
+    // var text = `<p class="content__main__name">${chooseMenu}</p>`;
+
+    for (const key in menues["menues"][language][chooseMenu]["text"]){
+        var line = menues["menues"][language][chooseMenu]["text"][key];
+
+        if (line == "----------------------------------------------------------------------------------------------------"){
+            line = "<br>";
+        } else if (line == ""){
+            line = "<br>";
+        }
+        
+        if (line[0] == "<" && line[-1] == ">"){
+            text += `${await holder(line)}`;
+        } else {
+            text += `<p class="content__main__discription">${await holder(line)}</p>`;
+        }
+    }
+
+    console.log(text);
+
+    mainClass.innerHTML = text;
+}
 
 async function initialization(){
-    languageDislay();
+    var en = await cacheLoadJSON("https://raw.githubusercontent.com/artyom7774/artyom7774.github.io/main/scr/translates/en.json");
+    var ru = await cacheLoadJSON("https://raw.githubusercontent.com/artyom7774/artyom7774.github.io/main/scr/translates/ru.json");
 
-    const contentClass = document.querySelector(".content");
-    const contentMenu = document.querySelector(".menu");
+    menues["menues"]["en"] = en;
+    menues["menues"]["ru"] = ru;
+
+    const menuClass = document.querySelector(".content__menu");
+
+    var text = "";
+
+    for (const key in menues["menu"]){
+        var menu = menues["menues"][language][key]["name"];
     
-    // INIT MENU ELEMENTS
-
-    help = await getHelpMenu()
-
-    text = "";
-
-    for (const name in help) {
-        variable = await translate(help[name]["name"])
-
-        text += `<li class="menu-submenu" id="${help[name]["name"]}">${variable}`;
-
-        index = 0;
-
-        for (const page in help[name]["pages"]) {
-            text += `<div class="menu-submenu-element">`;
-
-            if (Object.keys(help[name]["pages"]).length - 1 === index) {
-                text += `<svg height="30" width="26"><polyline points="10,0 10,15 25,15" style="fill:none;stroke:white;stroke-width:1" /></svg>`;
-            } else {
-                text += `<svg height="30" width="26"><polyline points="10,0 10,15 25,15 10,15 10,30" style="fill:none;stroke:white;stroke-width:1" /></svg>`;
-            }
-
-            variable = await translate(help[name]["pages"][page]["title"])
-
-            text += `<p class="menu-submenu-item" id="${help[name]["pages"][page]["title"]}">${variable}</p>`;
-            text += `</div>`;
-
-            index += 1;
+        if (menu == chooseMenu){
+            text += `<button class="content__menu__button" onclick="menuButtonFunction('${menu}')">• ${menu}</button>`;
+        } else {
+            text += `<button class="content__menu__button" onclick="menuButtonFunction('${menu}')">${menu}</button>`;
         }
 
-        text += `</li>`
+        if (Object.keys(menues["menu"][key]).length != 0){
+            var index = 0;
+
+            for (const element in menues["menu"][key]){
+                console.log(element);
+
+                if (index != Object.keys(menues["menu"][key]).length - 1){
+                    var char = "├";
+                } else {
+                    var char = "└";
+                }
+
+                if (`${menu}/${element}` == chooseMenu){
+                    text += `<button class="content__menu__button" onclick="menuButtonFunction('${menu}/${element}')">${char}&nbsp;• ${element}</button>`;
+                } else {
+                    text += `<button class="content__menu__button" onclick="menuButtonFunction('${menu}/${element}')">${char}&nbsp;${element}</button>`;
+                }
+
+                index += 1;
+            }
+        }
     }
 
-    contentMenu.innerHTML = text;
+    menuClass.innerHTML = text;
 
-    // SET START CONTENT TEXT
-
-    const submenu = document.getElementById("Game Engine 3");
-    const menu = submenu.parentElement.parentElement;
-
-    contentClass.innerHTML = await loadHelpMenu(menu.id, submenu.id);
-
-    // FINISH
-
-    addMenuEventListeners();
-};
-
-function languageDislay() {
-    if (language == "ru") {
-        document.getElementById("ru").checked = true;
-    } else if (language == "en") {
-        document.getElementById("en").checked = true;
-    }
+    mainInformationView();
 }
-
-function languageChooseRussian() {
-    language = "ru";
-
-    initialization();
-}
-
-function languageChooseEnglish() {
-    language = "en";
-
-    initialization();
-}
-
-function addMenuEventListeners() {
-    document.querySelectorAll(".menu-submenu-item").forEach(item => {
-        item.addEventListener("click", async event => {
-            const content = document.querySelector(".content");
-
-            const submenu = document.getElementById(event.currentTarget.id);
-            const menu = submenu.parentElement.parentElement;
-
-            const text = await loadHelpMenu(menu.id, submenu.id);
-            content.innerHTML = text;
-        });
-    });
-};
-
-// INITIALIZE
-
-let cacheLoadJSON = cache(loadJSON);
-let cacheGetBundle = cache(getBundle)
-
-// VARIABLES
-
-var language = "en";
-
-// START
 
 document.addEventListener('DOMContentLoaded', function () {
     initialization();
 });
+
+let cacheLoadJSON = cache(loadJSON);
